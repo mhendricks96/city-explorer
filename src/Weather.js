383af272756b5d4e7bbd43460c6ecb464cbc4868
mhendricks96 <mhendricks96@yahoo.com>
2021-04-13 14:09:45 -0700

    added error handling

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,29 @@
 import React from 'react';
+//import { ListGroup } from 'react-bootstrap';
 
 
 class Weather extends React.Component {
   
-  constructor(props){
-    super(props);
-    this.state = {};  
-    
-  }
+
   
   render(){
+
+    //let renderingWeather = this.props.cityWeather.map((day, index) => (
+      //<ListGroup.Item key={index}>{`${day.date}: ${day.description}`}</ListGroup.Item>
+  //))
+
+
+    console.log('in weatherjs', this.props.cityWeather)
     return(
+      //<ListGroup>
+        //{renderingWeather}
+      //</ListGroup>
       <div>
-        <p>Today's date: {this.props.firstDayDate}</p>
-        <p>Overall Weather Today: {this.props.firstDayDescription}</p>
-        <p>Today's High Temp: {this.props.firstDayHi}</p>
-        <p>Today's Low Temp: {this.props.firstDayLow}</p>
+      <p>Date: {this.props.cityWeather[0].date}</p>
+      <p>Todays Weather: {this.props.cityWeather[0].description}</p>
+      <p>High: {this.props.cityWeather[0].highTemp}</p>
+      <p>Low: {this.props.cityWeather[0].lowTemp}</p>
+      
       </div>
     )
   }
